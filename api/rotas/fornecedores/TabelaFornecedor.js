53228ee1279d1a8441e1e7105eaf24493d50f97c
Leandro Alves <leandrojoapi@gmail.com>
2021-03-27 23:58:55 -0300

    Sequelize FineOne - Buscando por um fornecedor

--- api/rotas/fornecedores/TabelaFornecedor.js
+++ api/rotas/fornecedores/TabelaFornecedor.js
@@ -3,9 +3,24 @@ const Modelo = require('./ModeloTabelaFornecedor')
 // Traduzindo o metodo findAll do Sequelize
 module.exports = {
     listar() {
+        // Lista todos os fornecedores
         return Modelo.findAll()
     },
     inserir(fornecedor) {
+        // Cria um novo fornecedor
         return Modelo.create(fornecedor)
+    },
+    async pegarPorId(id) {
+        // Retorna um fornecedor em especifico
+        const encontrado = await Modelo.findOne({
+            where: { id: id }
+        })
+
+        // Caso não tenha sido encontrado
+        if (!encontrado) {
+            throw new Error('ERROR! Fornecedor não encontrado')
+        }
+
+        return encontrado
     }
 }
