ca801a4dc815cc8fc3de1564ef292a1fa4738e0c
Leandro Alves <leandrojoapi@gmail.com>
2021-03-28 04:57:43 -0300

    Definindo formatos de dados aceitos

--- api/Serializador.js
+++ api/Serializador.js
@@ -2,6 +2,7 @@ const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
 
 class Serializador {
     serializar(dados) {
+        // Verifica se o conteudo recebido é do tipo JSON
         if (this.contentType === 'application/json') {
             return this.json(dados)
         }
@@ -9,7 +10,13 @@ class Serializador {
         throw new ValorNaoSuportado(this.contentType)
     }
 
+    // Converte os dados recebidos para JSON
     json(dados) {
         return JSON.stringify(dados)
     } 
+}
+
+module.exports = {
+    Serializador: Serializador,
+    formatosAceitos: ['application/json']
 }
