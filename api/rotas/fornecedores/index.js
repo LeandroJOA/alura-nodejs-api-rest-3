68f1d72d77e2934937cb08e122cf6e1d5d4138ad
Leandro Alves <leandrojoapi@gmail.com>
2021-03-27 23:55:57 -0300

    Rota para buscar um fornecedor em especifico

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -2,7 +2,7 @@ const roteador = require('express').Router()
 const TabelaFornecedor = require('./TabelaFornecedor')
 const Fornecedor = require('./Fornecedor')
 
-// Criando rota para listar todos os fornecedores 
+// Rota para listar todos os fornecedores 
 roteador.get('/', async (req, res) => {
     // Espera o metodo listar ser realizado
     const resultados = await TabelaFornecedor.listar()
@@ -14,13 +14,39 @@ roteador.get('/', async (req, res) => {
 
 // Rota para cadastro de fornecedores
 roteador.post('/', async (req, res) => {
+    // Capturando os dados enviados
     const dadosRecebidos = req.body
     const fornecedor = new Fornecedor(dadosRecebidos)
+
+    //Espera o metodo criar ser executado
     await fornecedor.criar()
     
+    // Respondendo com os dados adicionados
     res.send(
         JSON.stringify(fornecedor)
     )
+})
+
+// Rota para listar um unico fornecedor
+roteador.get('/:idFornecedor',async (req, res) => {
+    try {
+        // Capturando o parametro de id
+        const id = req.params.idFornecedor
+        const fornecedor = new Fornecedor({ id: id })
+
+        // Espera o metodo carregar ser executado
+        await fornecedor.carregar()
+
+        res.send(
+            JSON.stringify(fornecedor)
+        )
+    } catch (error) {
+        res.send(
+            JSON.stringify({
+                mensagem: error.message
+            })
+        )
+    }
 })
 
 module.exports = roteador
