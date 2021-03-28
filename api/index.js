75b6522acc7693030743bd8b92135c4c8443427c
Leandro Alves <leandrojoapi@gmail.com>
2021-03-27 21:54:34 -0300

    Utilizando o roteador para criar a rota /fornecedores

--- api/index.js
+++ api/index.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const config = require('config')
+const roteador = require('./rotas/fornecedores')
 
 app.use(bodyParser.json())
+
+app.use('/api/fornecedores', roteador)
 
 // config.get - Captura a porta da API, a partir do arquivo json de config
 app.listen(config.get('api.porta'), () => console.log('A api está funcionando!'))
