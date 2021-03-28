984e7e62a54d05eacf892fde6a7f784ba6cf53e5
Leandro Alves <leandrojoapi@gmail.com>
2021-03-28 04:58:20 -0300

    Verificando se o formato é valido antes da execução das APIs

--- api/index.js
+++ api/index.js
@@ -7,10 +7,37 @@ const NaoEncontrado = require('./erros/NaoEncontrado')
 const CampoInvalido = require('./erros/CampoInvalido')
 const DadosNaoFornecidos = require('./erros/DadosNaoFornecidos')
 const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
+const formatosAceitos = require('./Serializador').formatosAceitos
 
 const app = express()
 
 app.use(bodyParser.json())
+
+// Verifica se o conteudo recebido é valido 
+app.use((req, res, proximo) => {
+    // Identifica o formato de dados recebido
+    let formatoRequisitado = req.header('Accept')
+
+    // Se o formato for do tipo padrão "*/*" (get), transforma este em JSON
+    if (formatoRequisitado === '*/*') {
+        formatoRequisitado = 'application/json'
+    }
+
+    // Verifica se o formato recebido não está entre os aceitos
+    if (formatosAceitos.indexOf(formatoRequisitado) === -1) {
+        // Status Not Acceptable
+        res.status(406)
+        // Finaliza a requisição
+        res.end()
+        // Termina a execução do codigo
+        return
+    }
+
+    // Seta o formato de resposta para o mesmo que o recebido
+    res.setHeader('Content-Type', formatoRequisitado)
+    // Avança para o proximo middleware
+    proximo()
+})
 
 app.use('/api/fornecedores', roteador)
 
