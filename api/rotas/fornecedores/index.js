64bab73ddf73a0077688e590934a6cee21e56d66
Leandro Alves <leandrojoapi@gmail.com>
2021-03-27 23:36:59 -0300

    Criando rota para cadastro de fornecedores

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -1,13 +1,25 @@
 const roteador = require('express').Router()
 const TabelaFornecedor = require('./TabelaFornecedor')
+const Fornecedor = require('./Fornecedor')
 
 // Criando rota para listar todos os fornecedores 
-roteador.use('/', async (req, res) => {
+roteador.get('/', async (req, res) => {
     // Espera o metodo listar ser realizado
     const resultados = await TabelaFornecedor.listar()
     // Retorna os resultados como JSON
     res.send(
         JSON.stringify(resultados)
+    )
+})
+
+// Rota para cadastro de fornecedores
+roteador.post('/', async (req, res) => {
+    const dadosRecebidos = req.body
+    const fornecedor = new Fornecedor(dadosRecebidos)
+    await fornecedor.criar()
+    
+    res.send(
+        JSON.stringify(fornecedor)
     )
 })
 
