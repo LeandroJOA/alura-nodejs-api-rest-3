ef56856c8232a3b36544cc5e3c72c5c094103a17
Leandro Alves <leandrojoapi@gmail.com>
2021-03-28 23:10:28 -0300

    Incluindo suporte a XML

--- api/Serializador.js
+++ api/Serializador.js
@@ -1,16 +1,24 @@
+const jsontoxml = require('jsontoxml')
+
 const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
 
 class Serializador {
 
     serializar(dados) {
+        // Filtra os dados recebidos, retornando apenas os campos publicos e extras
+        dados = this.filtrar(dados)
+
         // Verifica se o conteudo recebido é do tipo JSON
         if (this.contentType === 'application/json') {
             // Passo pro metodo json os campos já filtrados
-            return this.json(
-                this.filtrar(dados)
-            )
+            return this.json(dados)
+        // Verifica se é do tipo XML    
+        } else if (this.contentType === 'application/xml') {
+            // Passo pro metodo xml os campos já filtrados
+            return this.xml(dados)
         }
 
+        // Caso não seja nenhum das opções acima, lança um error
         throw new ValorNaoSuportado(this.contentType)
     }
 
@@ -46,6 +54,26 @@ class Serializador {
     json(dados) {
         return JSON.stringify(dados)
     } 
+
+    // Converte os dados recebidos para XML
+    xml(dados) {
+        // Nomo do campo no singular
+        let tag = this.tagSingular
+
+        // Verifica se os dados a serem retornados são um lista
+        if (Array.isArray(dados)) {
+            // Utiliza a tag no plural
+            tag = this.tagPlural
+            // Mapea todos os dados da lista, criando um novo objeto com todos os fornecedores separados 
+            dados = dados.map((item) => {
+                return {
+                    [this.tagSingular]: item
+                }
+            })
+        }
+
+        return jsontoxml({ [tag]: dados })
+    }
 }
 
 class SerializadorFornecedor extends Serializador {
@@ -56,6 +84,8 @@ class SerializadorFornecedor extends Serializador {
         this.contentType = contentType  
         // Concatena os campos que podem ser retornados com os campos extras (caso não exista, usa uma lista vazia)
         this.camposPublicos = ['id', 'empresa', 'categoria'].concat(camposExtras || [])
+        this.tagSingular = 'fornecedor'
+        this.tagPlural = 'fornecedores'
     }
 }
 
@@ -66,6 +96,8 @@ class SerializadorErro extends Serializador {
         super()
         this.contentType = contentType
         this.camposPublicos = ['id', 'mensagem'].concat(camposExtras || [])
+        this.tagSingular = 'error'
+        this.tagPlural = 'errors'
     }
 }
 
@@ -73,5 +105,5 @@ module.exports = {
     Serializador: Serializador,
     SerializadorFornecedor: SerializadorFornecedor,
     SerializadorErro: SerializadorErro,
-    formatosAceitos: ['application/json']
+    formatosAceitos: ['application/json', 'application/xml']
 }
